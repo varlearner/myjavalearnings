3508f3d3e262456a42ea85aab08e6fe03788fe40
varlearner <150174717+varlearner@users.noreply.github.com>
2023-11-07 15:33:19 -0500

    Given string is reversed and printed

--- src/com/learnjava/ReverseString.java
+++ src/com/learnjava/ReverseString.java
@@ -6,10 +6,10 @@ public class ReverseString {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your string");
-        String pali = sc.next();
+        String rev = sc.next();
 
-        for (int index = pali.length()- 1; index >= 0; index--) {
-            char chars = pali.charAt(index);
+        for (int index = rev.length()- 1; index >= 0; index--) {
+            char chars = rev.charAt(index);
             System.out.print(chars);
         }
     }
