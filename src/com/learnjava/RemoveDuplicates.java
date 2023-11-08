f35bf4276dead10a4d85bf94a1ae0cbcbb34edf1
varlearner <elativarshareddy@gmail.com>
2023-11-08 18:07:32 -0500

    Program to remove duplicates from a given array and print unique values.

---
+++ src/com/learnjava/RemoveDuplicates.java
@@ -0,0 +1,46 @@
+package src.com.learnjava;
+
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("enter size" );
+        int size = scanner.nextInt();
+        int[] arr1 = new int[size];
+
+        for (int i=0, j=1; i<size ; i++, j++) {
+            System.out.println("Enter number " + j + ": " );
+            arr1[i] = scanner.nextInt();
+        }
+
+        if(arr1.length == 0) {
+            return;
+        }
+
+        if(arr1.length == 1) {
+            System.out.println(arr1[0]);
+            return;
+        }
+
+        int[] finalArr = new int[arr1.length];
+
+        finalArr[0] = arr1[0];
+
+        int j=1, currNum = arr1[0];
+
+        for ( int i = 0; i < arr1.length; i++) {
+
+            if (currNum != arr1[i]) {
+                finalArr[j] = arr1[i];
+                j++;
+            }
+            currNum = arr1[i];
+        }
+        for (int i = 0; i < j; i++) {
+            System.out.println(finalArr[i]);
+        }
+    }
+}
