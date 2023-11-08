c0698f55ff7414ea3fa80f29abf4b32d21e43f48
varlearner <elativarshareddy@gmail.com>
2023-11-08 12:39:22 -0500

    To check if a given string is Palindrome or a non-palindrome

--- src/com/learnjava/CheckPalindrome.java
+++ src/com/learnjava/CheckPalindrome.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class CheckPalindrome {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your string");
         String pali = sc.next();
